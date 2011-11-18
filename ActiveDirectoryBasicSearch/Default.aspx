a0f514aaf6d8afa834c2f721b511d568a4835402
Matt Andreko <mandreko@gmail.com>
2011-11-18 08:15:02 -0500

    Initial commit

---
+++ ActiveDirectoryBasicSearch/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ActiveDirectoryBasicSearch.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="search">
+            Name: <asp:TextBox runat="server" ID="nameTextBox" ></asp:TextBox>
+            
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                                        ErrorMessage="Name is required." ControlToValidate="nameTextBox"></asp:RequiredFieldValidator>
+
+            <asp:Button runat="server" ID="submitButton" Text="Submit" onclick="submitButton_Click"/>
+        </div>
+        <div id="searchResults">
+            <asp:GridView ID="searchResultsGrid" runat="server" AutoGenerateColumns="False" 
+                EmptyDataText="No Contacts Found">
+                <Columns>
+                    <asp:BoundField DataField="DisplayName" HeaderText="Display Name" />
+                    <asp:BoundField DataField="Phone" HeaderText="Phone" />
+                    <asp:BoundField DataField="Office" HeaderText="Office" />
+                    <asp:BoundField DataField="Ext" HeaderText="Ext."/>
+                </Columns>
+            </asp:GridView>
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ ActiveDirectoryBasicSearch/Default.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.DirectoryServices;
+using System.Text;
+
+namespace ActiveDirectoryBasicSearch
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the Click event of the submitButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(nameTextBox.Text))
+            {
+                DataTable dt = GetModifiedUsers(nameTextBox.Text);
+
+                searchResultsGrid.DataSource = dt;
+                searchResultsGrid.DataBind();
+            }
+        }
+
+        // Method stolen from http://www.dotnettreats.com/tipstricks/adnet.aspx (tweaked slightly)
+        /// <summary>
+        /// Method used to create an entry to the AD.
+        /// Replace the path, username, and password.
+        /// </summary>
+        /// <returns>DirectoryEntry</returns>
+        public static DirectoryEntry GetDirectoryEntry(string path = null)
+        {
+            string useSecureConnectionString = ConfigurationManager.AppSettings["UseSecureConnection"];
+            bool useSecureConnection;
+            bool useSecureConnectionStringParsedSuccessfully = bool.TryParse(useSecureConnectionString, out useSecureConnection);
+
+            if (!useSecureConnectionStringParsedSuccessfully)
+            {
+                throw new ConfigurationErrorsException("UseSecureConnection was not specified in the Web.config as 'true' or 'false'");
+            }
+
+            DirectoryEntry de = new DirectoryEntry
+            {
+                Path = path ?? GetLdapString()
+            };
+
+            if (useSecureConnection)
+            {
+                de.AuthenticationType = AuthenticationTypes.Secure;
+            }
+            else
+            {
+                de.Username = ConfigurationManager.AppSettings["LDAPUsername"];
+                de.Password = ConfigurationManager.AppSettings["LDAPPassword"];
+            }
+            
+            return de;
+        }
+
+        // Method stolen from http://www.dotnettreats.com/tipstricks/adnet.aspx
+        /// <summary>
+        /// Method that returns a DataTable with a list of users modified from a given date.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public DataTable GetModifiedUsers(string name)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("DisplayName");
+            dt.Columns.Add("Phone");
+            dt.Columns.Add("Office");
+            dt.Columns.Add("Ext");
+
+            DirectoryEntry de = GetDirectoryEntry();
+            DirectorySearcher ds = new DirectorySearcher(de);
+
+            StringBuilder filter = new StringBuilder();
+            filter.Append("(&(objectCategory=Person)(objectClass=user)(givenName=");
+            filter.Append(name);
+            filter.Append("))");
+
+            ds.Filter = filter.ToString();
+            ds.SearchScope = SearchScope.Subtree;
+            SearchResultCollection results = ds.FindAll();
+
+            foreach (SearchResult result in results)
+            {
+                DataRow dr = dt.NewRow();
+                DirectoryEntry dey = GetDirectoryEntry(result.Path);
+                dr["DisplayName"] = dey.Properties["cn"].Value;
+                dr["Phone"] = dey.Properties["telephoneNumber"].Value;
+                dr["Office"] = dey.Properties["department"].Value;
+                dr["Ext"] = dey.Properties["telephoneNumber"].Value;
+                dt.Rows.Add(dr);
+                dey.Close();
+            }
+
+            de.Close();
+            return dt;
+        }
+
+        /// <summary>
+        /// Gets the LDAP string.
+        /// </summary>
+        /// <returns></returns>
+        private static string GetLdapString()
+        {
+            string ldapServer = ConfigurationManager.AppSettings["LDAPServer"];
+            string ldapPath = ConfigurationManager.AppSettings["LDAPPath"];
+
+            if (string.IsNullOrEmpty(ldapServer))
+            {
+                throw new ConfigurationErrorsException("LDAPServer was not specified in the Web.config");
+            }
+
+            if (string.IsNullOrEmpty(ldapPath))
+            {
+                throw new ConfigurationErrorsException("LDAPPath was not specified in the Web.config");
+            }
+
+            return string.Format("LDAP://{0}/{1}", ldapServer, ldapPath);
+        }
+    }
+}
